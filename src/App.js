86affb18873d43d7637a11e3b63555068e6e4893
mameaw14 <pinyapornster@gmail.com>
2017-11-28 22:43:40 +0700

    Deploy

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     result: []
   };
 
-  handleChange = event => {
+  handleChange = event => { 
     let ip = event.target.value;
     this.setState({
       ip: ip,
