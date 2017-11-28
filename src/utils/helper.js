1333dcc84bfab307de395efe3f0f1c33d057f6fb
mameaw14 <pinyapornster@gmail.com>
2017-11-28 19:26:31 +0700

    Add Function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -8,3 +8,54 @@ export const convertToSubnet = (mask) => {
     });
     return subnet.join('.');
 }
+
+export const toBinary = (ip) => {
+    const binary = ip.split('.').map((elem) => {
+        const a = (+elem).toString(2);
+        return '0'.repeat(8 - a.length) + a;
+    });
+    return binary.join('.');
+}
+
+export const networkAddress = (ip, subnet) => {
+    ip = toBinary(ip);
+    const a = ip.split('.').map((element) => {
+        const b = element.split('').map((digit) => {
+            subnet -= 1;
+            return subnet >= 0? digit : '0';
+        });
+        return parseInt(b.join(''), 2);
+    });
+    return a.join('.');
+}
+
+export const broadcastAddress = (ip, subnet) => {
+    ip = toBinary(ip);
+    const a = ip.split('.').map((element) => {
+        const b = element.split('').map((digit) => {
+            subnet -= 1;
+            return subnet >= 0? digit : '1';
+        });
+        return parseInt(b.join(''), 2);
+    });
+    return a.join('.');
+}
+
+export const numberOfHosts = (subnet) => {
+    return Math.pow(2, 32-subnet);
+}
+
+export const numberOfUsableHosts = (subnet) => {
+    return Math.pow(2, 32-subnet) - 2;
+}
+
+export const wildCard = (mask) => {
+    const subnet = [0, 0, 0, 0].map(() => {
+        const sub = '00000000'.split('').map(() => {
+            mask -= 1;
+            return mask >= 0 ? '0' : '1';
+        });
+        return parseInt(sub.join(''), 2);
+    });
+    return subnet.join('.');
+}
--- src/utils/helper.test.js
+++ src/utils/helper.test.js
@@ -1,9 +1,56 @@
-import { convertToSubnet } from './helper';
 import { expect } from 'chai';
+import { 
+    convertToSubnet, 
+    networkAddress,
+    toBinary,
+    broadcastAddress,
+    numberOfHosts,
+    numberOfUsableHosts,
+    wildCard
+} from './helper';
 
 describe('convertToSubnet test', () => {
     it('should return subnet mask', () => {
         expect(convertToSubnet(32)).to.equal('255.255.255.255');
+    });
+})
+
+describe('wildcard test', () => {
+    it('should return wildcard', () => {
+        expect(wildCard(32)).to.equal('0.0.0.0');
+    });
+})
+
+describe('networkAddress test', () => {
+    it('should return network address', () => {
+        expect(networkAddress('255.255.255.255',1)).to.equal('128.0.0.0');
+        expect(networkAddress('255.255.255.255',8)).to.equal('255.0.0.0');
+        expect(networkAddress('234.255.255.255',8)).to.equal('234.0.0.0');
+    });
+})
+
+describe('toBinary test', () => {
+    it('should return binary format ip', () => {
+        expect(toBinary('128.0.0.0')).to.equal('10000000.00000000.00000000.00000000');
+    });
+})
+
+describe('broadcastAddress test', () => {
+    it('should return broadcast address', () => {
+        expect(broadcastAddress('108.104.24.1', 8)).to.equal('108.255.255.255');
+        expect(broadcastAddress('108.104.24.1', 9)).to.equal('108.127.255.255');
+    });
+})
+
+describe('numberOfHosts test', () => {
+    it('should return number of hosts', () => {
+        expect(numberOfHosts(9)).to.equal(8388608);
+    });
+})
+
+describe('numberOfUsableHosts test', () => {
+    it('should return number of usable hosts', () => {
+        expect(numberOfUsableHosts(9)).to.equal(8388606);
     });
 })
 
